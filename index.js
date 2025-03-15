bba0e1a869c952eb234400a32fda34223464a7f1
yuan12766 <1692530894@qq.com>
2025-03-15 21:09:22 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+// import { Hello,firstName,getName as getMyName } from "./hello";     //as改名
+// import * as MyHello from "./hello"
+
+// MyHello.getName();                          //统一加载
+// console.log(MyHello.firstName);
+
+// function getName(){
+//     console.log("哈哈哈");
+// }
+
+// getName();
+
+// getMyName();
+
+
+import CustomName from "./hello"
+CustomName();
