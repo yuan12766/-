b902d93382f40d5ca78bbf6d1e301543fb89e69d
yuan12766 <1692530894@qq.com>
2025-03-30 23:24:20 +0800

    Add files via upload

---
+++ 3.30.1事件传参 1.vue
@@ -0,0 +1,25 @@
+<template>
+    <h3>传递参数</h3>
+    <button @click="addCount">Add</button>
+    <p>{{ count }}</p>
+</template>
+
+<script>
+
+const { log } = console;
+
+export default {
+    data() {
+        return {
+            count: 0,
+        }
+    },
+    methods: {
+        addCount(e) {
+            // e.target.innerHTML = "Add " + this.count
+            this.count++
+        }
+    }
+}
+
+</script>
---
+++ 3.30.1事件传参 2.vue
@@ -0,0 +1,24 @@
+<template>
+    <h3>传递参数</h3>
+    <p @click="getNameHandle(item, $event)" v-for="(item, index) of names" :key="index">{{ item }}</p>
+</template>
+
+<script>
+
+const { log } = console;
+
+export default {
+    data() {
+        return {
+            names: ["qwe", "asd", "zxc"],
+        }
+    },
+    methods: {
+        getNameHandle(name, e) {
+            console.log(name);
+            console.log(e);
+        }
+    }
+}
+
+</script>
