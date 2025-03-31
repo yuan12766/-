93db1acedff7a31f2b4b56618f6e3f356e7c4bd7
yuan12766 <1692530894@qq.com>
2025-03-31 22:37:56 +0800

    Add files via upload

---
+++ 3.30.2事件修饰符.vue
@@ -0,0 +1,37 @@
+<template>
+    <h3>事件修饰符</h3>
+    <!-- 阻止事件默认 -->
+    <a @click.prevent="clickHandle" href="https://www.qidian.com/">起点</a>
+
+    <!-- 阻止事件冒泡 -->
+    <div @click="clickDiv">
+        <p @click.stop="clickP">测试冒泡</p>
+    </div>
+</template>
+<script>
+
+export default {
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+        clickHandle(e) {
+            // e.preventDefault();
+            console.log("点击了");
+
+        },
+        clickDiv() {
+            console.log("div");
+
+        },
+        clickP(e) {
+            // e.stopPropagation();
+            console.log("P");
+
+        }
+    }
+}
+
+</script>
