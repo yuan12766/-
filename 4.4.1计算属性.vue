9bc02062b596ff5817d38802c52e5d4a19a57430
yuan12766 <1692530894@qq.com>
2025-04-04 23:45:03 +0800

    Add files via upload

---
+++ 4.4.1计算属性.vue
@@ -0,0 +1,31 @@
+<template>
+    <h3>{{ yuan.name }}</h3>
+    <p>{{ yuancontent }}</p>
+    <p>{{ yuancontents() }}</p>
+</template>
+<script>
+
+export default {
+    data() {
+        return {
+            yuan: {
+                name: ["qwe", "asd", "zxc"],
+                content: ["前端", "Java", "Python"]
+            }
+        }
+    },
+    //计算属性
+    computed: {
+        yuancontent() {
+            return this.yuan.content.length > 0 ? 'Yes' : 'No'
+        }
+    },
+    // 放函数或方法
+    methods:{
+        yuancontents(){
+            return this.yuan.content.length > 0 ? 'Yes' : 'No'
+        }
+    }
+}
+
+</script>
